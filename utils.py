eb2a656630fcdba439bbd4f8f15bc0ec50abd029
nagar-mayank <nagar_mayank@iitgn.ac.in>
2021-02-13 22:21:53 +0530

    Initial Commit

---
+++ utils.py
@@ -0,0 +1,45 @@
+def crop(I, i):
+    dim = I.shape
+    I = I[i*64:i*64+64, i*64:i*64+64]
+    return I
+
+def jpeg_images(q, path):
+    for i in range(1, 1339):
+        img = 'ucid'+(str(i).zfill(5))+'.tif'
+        I = Image.open(img)
+        name, ext = img.split('.')
+        image = os.path.join(path+name+'.jpeg')
+        I.save(image,"JPEG", quality=q)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
